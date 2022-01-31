8aac9ab17a1d50a22a2f807491f35801b56c8710
dodgyd2001 <dodgyd2001@gmail.com>
2022-01-31 16:13:19 +0000

    Update CelestialObjectController.cs
    added actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,6 +17,63 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{id:int}",Name = "GetById")]    
+        public IActionResult GetById(int id)
+        {            
+            var celestial = _context.CelestialObjects.Where(c => c.Id == id).FirstOrDefault();            
+            if(celestial == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                celestial.Satellites = new List<Models.CelestialObject>();
+                celestial.Satellites
+                    .AddRange(_context.CelestialObjects.Where(s => s.OrbitedObjectId == celestial.Id));
+                return new OkObjectResult(celestial);
+            }
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestials = _context.CelestialObjects.Where(c => c.Name == name.Trim()).ToList();
+            if (celestials == null || celestials.Count ==0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                foreach(var celestial in celestials)
+                {
+                    celestial.Satellites = new List<Models.CelestialObject>();
+                    celestial.Satellites
+                        .AddRange(_context.CelestialObjects.Where(s => s.OrbitedObjectId == celestial.Id));
+                }                               
+                return new OkObjectResult(celestials);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestials = _context.CelestialObjects.ToList();
+            if (celestials == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                foreach (var celestial in celestials)
+                {
+                    celestial.Satellites = new List<Models.CelestialObject>();
+                    celestial.Satellites
+                        .AddRange(_context.CelestialObjects.Where(s => s.OrbitedObjectId == celestial.Id));
+                }
+
+                return new OkObjectResult(celestials);
+            }
+        }
+
         public IActionResult Index()
         {
             return View();
